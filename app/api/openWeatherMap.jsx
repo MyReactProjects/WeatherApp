23ccff18adc2fe289ee1e31a3ab83c1f6c8c3afb
yuda85 <yuda8855@gmail.com>
2017-05-09 21:56:26 -0600

    initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,20 @@
+var axios = require('axios')
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=e596828d2a446010e2548bee02b4e40a&units=imperial'
+//   e596828d2a446010e2548bee02b4e40a api key
+module.exports = {
+  getTemp: function(location) {
+    var encodedLocation = encodeURIComponent(location)
+    var requesUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`
+
+    return axios.get(requesUrl).then(function(res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function(res) {
+      throw new Error(res.data.message);
+    });
+  }
+} // end exports
